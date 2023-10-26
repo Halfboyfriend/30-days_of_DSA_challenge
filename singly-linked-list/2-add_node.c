0fd11a74227a24f31731c0d422f4a459b0cb1502
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-10-26 11:01:21 +0100

    add

--- singly-linked-list/2-add_node.c
+++ singly-linked-list/2-add_node.c
@@ -3,7 +3,7 @@
 list_t *add_node(list_t **head, const char *str){
     list_t *newnode;
     newnode = malloc(sizeof(list_t));
-    newnode->str = str;
-    newnode->next = head;
-    head = newnode;
+    newnode->str = strdup(str);
+    newnode->next = *head;
+    *head = newnode;
 }
