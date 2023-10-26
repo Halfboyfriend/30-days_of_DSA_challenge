bab10efd28fb36fff8eac057f481e33718ab036a
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-10-26 10:55:33 +0100

    add

--- singly-linked-list/1-list_len.c
+++ singly-linked-list/1-list_len.c
@@ -1,5 +1,18 @@
-#include <stdio.h>
+#include "lists.h"
 
-int main(){
-    printf("Welcome");
+size_t list_len(const list_t *h){
+   const list_t *temp;
+    size_t count;
+
+    temp = h;
+    if(temp == NULL){
+        return(count);
+    }
+    while(temp != NULL){
+        temp = temp -> next;
+        count++;
+    }
+    
+    return(count);
+
 }
---
+++ singly-linked-list/2-add_node.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+
+list_t *add_node(list_t **head, const char *str){
+    list_t *newnode;
+    newnode = malloc(sizeof(list_t));
+    newnode->str = str;
+    newnode->next = head;
+    head = newnode;
+}
